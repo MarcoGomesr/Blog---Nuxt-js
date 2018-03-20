37475ebef2815aea771670eb3a42e9c051c1b292
Marco <marco@whipgo.com>
2018-03-20 01:01:28 +0000

    create header

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <main class="main-content">
+      <nuxt/>
+    </main>
+  </div>
+</template>
+
+<style>
+*{
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'Lato', sans-serif;
+}
+
+.main-content{
+  margin-top: 4.5rem;
+}
+</style>
---
+++ pages/about/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <section id="about-page">
+      <h1>H1, that us!</h1>
+      <p>We'r a bunch aof highly motivated talented people</p>
+  </section>
+</template>
